a169a774f64e682983918b5d5947ab84dd03289b
Sophia Katsuno <chiaki.katsuno@live.ca>
2023-05-29 14:57:37 -0700

    ice cream arrays

---
+++ ice-cream.js
@@ -0,0 +1,18 @@
+// Do not modify these first two lines
+const iceCreamFlavours = ["chocolate", "vanilla", "cookies and cream", "rocky road", "strawberry"];
+console.log(iceCreamFlavours);
+
+// Your code below here...
+
+// add root beer as a new flavor 
+iceCreamFlavours.push("root beer")
+console.log(iceCreamFlavours)
+
+// print first flavor 
+console.log(iceCreamFlavours[0]);
+
+//obtain last flavor
+console.log(iceCreamFlavours[iceCreamFlavours.length-1])
+
+// total # of flavors
+console.log(iceCreamFlavours.length)
