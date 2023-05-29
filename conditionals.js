be6036f271e833be1fda73163d22b6d793a81c5e
Sophia Katsuno <chiaki.katsuno@live.ca>
2023-05-28 18:03:11 -0700

    changes

--- conditionals.js
+++ conditionals.js
@@ -1,4 +1,4 @@
-const temperature = ;
+const temperature = 
 
 if (temperature < 0) {
   console.log("Make sure you pick out a scarf!");
@@ -9,3 +9,4 @@ if (temperature < 0) {
 }
 
 console.log("Now you're ready to go outside!");
+
